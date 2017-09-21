0befa662dd360cccd06be099d92f000d8d860b80
Bob Rudis <bob@rud.is>
2017-09-21 16:37:17 -0400

    Got some boilerplate items out of the way for later on and also added just enough bits to get an intial CRAN check passing.

---
+++ R/fishpals-package.R
@@ -0,0 +1,15 @@
+#' Species-specific Fish Color Palettes for Plots
+#'
+#' Modeled after Karthik Ram's [`wesanderson`](https://CRAN.R-project.org/package=wesanderson)
+#' palettes package, methods areprovided to create species-specific color palettes generated
+#' from pictures of fish that [Cramer Fish Sciences](http://www.fishsciences.net/) (CFS)
+#' has worked with in the past. Since CFS works mostly with North American, northwestern
+#' fish species, we're talking about a lot of greens, browns, and grays. But we're open
+#' to adding some tropical palettes.
+#'
+#' @md
+#' @name fishpals
+#' @docType package
+#' @author Myfanwy Johnston <myfanwy.johnston@@fishsciences.net>
+#' @importFrom grDevices colorRampPalette
+NULL
