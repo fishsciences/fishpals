36688b29091bc84badb405f34fc0f1755cfd9404
Myfanwy <mrowlan1@gmail.com>
2019-03-19 15:43:40 -0700

    update README, modify theme_pres

--- R/themes.R
+++ R/themes.R
@@ -44,7 +44,7 @@ theme_pres <- function () {
       legend.background = element_rect(fill="transparent", colour=NA),
       legend.key = element_rect(fill="transparent", colour=NA),
 
-     panel.grid.major = element_line("grey60", size = rel(0.75)),
+     panel.grid.major = element_line("grey60"),
       panel.grid.minor = element_line("grey60",size = rel(0.5)),
       complete = TRUE
     )
