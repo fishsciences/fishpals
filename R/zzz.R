f26f8ee955c7fcee94420c7b2b7c46148d464b23
Myfanwy <myfanwy@github.com>
2019-07-16 15:42:52 -0700

    remove font messages

--- R/zzz.R
+++ R/zzz.R
@@ -5,13 +5,6 @@
     extrafont::loadfonts("win", quiet = TRUE)
   }
 
-  if (interactive()) packageStartupMessage("Registering PDF & PostScript fonts with R")
-  extrafont::loadfonts("pdf", quiet = TRUE)
-  extrafont::loadfonts("postscript", quiet = TRUE)
-
-  fnt <- extrafont::fonttable()
-  if (!any(grepl("Avenir", fnt$FamilyName))) {
-    packageStartupMessage("NOTE: The Avenir font is required to use the full functionality of these themes.")
-  } # nocov end
+ # nocov end
 
 }
