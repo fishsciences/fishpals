029b39e2dcbbfd9d3fbeae31b1e4741bb162fa3d
Myfanwy <mrowlan1@gmail.com>
2019-07-17 13:24:13 -0700

    add foundationfp theme; update report theme

--- R/themes.R
+++ R/themes.R
@@ -3,11 +3,14 @@
 #'@description lightweight ggplot2 themes for Cramer Fish Sciences and fish lovers alike
 #'
 #'\describe{
+#'\item{theme_foundation}{Code modified from [jrnold's theme_foundation](https://github.com/jrnold/ggthemes/blob/master/R/theme-foundation.R); designed to be a foundation from which the other themes are built, and not meant to be used directly.}
 #'\item{theme_report}{has gridlines}
-#'\item{theme_pres}{intended for use in presentations. Larger base_font}
+#'\item{theme_pres}{intended for use in presentations. Dark theme, larger base_font}
 #'\item{theme_pub}{good template for publications}
 #'}
+#'
 #'@export
+#'@importFrom ggplot2 theme_grey
 #'@examples
 #' library(ggplot2)
 #'
@@ -23,26 +26,56 @@
 #'  scale_color_fishpals(discrete = FALSE, palette = "genidaqs") +
 #'  theme_pub()
 #'
-theme_report <- function () {
+theme_foundationfp <- function(base_size = 11, base_family = "Geneva") {
+  thm <- theme_grey(base_size = base_size, base_family = base_family)
+  for (i in names(thm)) {
+    if ("colour" %in% names(thm[[i]])) {
+      thm[[i]]["colour"] <- list(NULL)
+    }
+    if ("fill" %in% names(thm[[i]])) {
+      thm[[i]]["fill"] <- list(NULL)
+    }
+  }
+  thm + theme(line = element_line(colour = "black"),
+              rect = element_rect(fill = "transparent", colour = NA),
+              text = element_text(colour = "black"),
+              panel.background = element_rect(color = NA),
+              legend.background = element_rect(colour = NA),
+              )
+}
 
-  theme_minimal(base_size = 12, base_family = "Geneva") %+replace%
+
+theme_report <- function(border = TRUE) {
+
+  ret <- theme_foundationfp() %+replace%
     theme(
-      panel.background  = element_blank(),
-      legend.background = element_rect(fill = "transparent", colour = NA),
-      legend.key = element_rect(fill = "transparent", colour = NA),
+      panel.border = element_rect(colour = "gray25", size = rel(1.20)),
+      plot.margin = margin(7.5, 7.5, 7.5, 7.5),
+      legend.background = element_rect(fill = "transparent",
+                                       colour = NA),
+      legend.key = element_rect(fill = "transparent",
+                                colour = NA),
       panel.grid.major = element_line("grey75",
                                       size = rel(0.4)),
       panel.grid.minor = element_line("grey75",
                                       size = rel(0.15)),
-      panel.spacing.x = unit(2, "lines"),
-      strip.text = element_text(size = 10, face = "bold"),
+      panel.spacing.x = unit(5, "lines"),
+      strip.text = element_text(size = 10,
+                                face = "bold",
+                                margin = margin(t = 7.5, r = 0, b = 5.5, l = 0)),
       strip.background = element_rect(fill = "gray95",
                                       color = "transparent",
-                                      size = rel(0.5)),
-        axis.line = element_line(size = 0.4),
+                                      size = rel(0.75)),
       axis.text = element_text(size = 13),
-      complete = TRUE
-    )
+      axis.title = element_text(size = 14),
+
+      complete = TRUE )
+
+  if(border == FALSE) {
+    ret <- ret + theme(panel.border = element_rect(colour = NA),
+                       axis.ticks = element_blank())
+  }
+  ret
 }
 
 #' @rdname theme_report
--- tests/testthat/test_fonts.R
+++ tests/testthat/test_fonts.R
@@ -1,17 +1,23 @@
 # context("fonts")
+# the structure of a theme is a list of lists
 
 library(fishpals)
 
 
 ggplot(mtcars) +
-  geom_point(aes(x = disp, y = hp), alpha = 0.2) +
+  geom_point(aes(x = disp, y = hp)) +
   theme_report()
+
+ggplot(mtcars) +
+  geom_point(aes(x = disp, y = hp)) +
+  theme(plot.margin = unit(50, "pt"))
 
 ggplot(iris) +
   geom_bar(aes(x = Species, y = Sepal.Width, fill = Species), stat = "identity") +
   theme_report()
 
 ggplot(mtcars) +
-   geom_point(aes(x = disp, y = hp), alpha = 0.2) +
+   geom_point(aes(x = disp, y = hp)) +
   facet_wrap(~am, scales = "free" ) +
   theme_report()
+
