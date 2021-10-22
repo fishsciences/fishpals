5aa49ea300897ce56cc589aa38a2458c6cf48d84
Myfanwy <myfanwy@github.com>
2021-10-22 14:23:27 -0700

    remove unused code, add transparency arg to theme_pres

--- R/themes.R
+++ R/themes.R
@@ -21,24 +21,17 @@
 #'   scale_color_fishpals("genidaqs") +
 #'   theme_report()
 #'
-#' ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Sepal.Length)) +
-#'  geom_point(size = 4, alpha = .6) +
-#'  scale_color_fishpals(discrete = FALSE, palette = "genidaqs") +
-#'  theme_pub()
-#'
+ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Sepal.Length)) +
+   geom_point(size = 4, alpha = .6) +
+   scale_color_fishpals(discrete = FALSE, palette = "genidaqs") +
+   theme_pres()
+
+
 theme_foundationfp <- function(base_size = 11, base_family = "Geneva") {
   thm <- theme_grey(base_size = base_size, base_family = base_family)
-  for (i in names(thm)) {
-    if ("colour" %in% names(thm[[i]])) {
-      thm[[i]]["colour"] <- list(NULL)
-    }
-    if ("fill" %in% names(thm[[i]])) {
-      thm[[i]]["fill"] <- list(NULL)
-    }
-  }
-  thm + theme(line = element_line(colour = "black"),
-              rect = element_rect(fill = "transparent", colour = NA),
-              text = element_text(colour = "black"),
+
+  # these additions to the foundation theme just remove some of the defaults of theme_gray and set them to transparent/NA/NULL to for other themes to iterate on
+  thm + theme(
               panel.background = element_rect(color = NA),
               legend.background = element_rect(colour = NA),
               )
@@ -46,76 +39,120 @@ theme_foundationfp <- function(base_size = 11, base_family = "Geneva") {
 
 #' @export
 #'
-theme_report <- function(inner_border = TRUE, outer_border = TRUE,
+theme_report <- function(inner_border = TRUE,
+                         outer_border = TRUE,
                          outer_border_lwd = 0.5,
                          outer_border_color = "black") {
-
   ret <- theme_foundationfp() %+replace%
+    # replace elements of theme_gray() with the following:
     theme(
-     panel.border = element_rect(colour = "gray25", size = rel(1.20)),
-      plot.margin = margin(7.5, 7.5, 7.5, 7.5),
+      panel.border = element_rect(colour = "gray25",
+                                  size = rel(1.20)),
+
+      plot.margin = margin(t = 7.5,
+                           r = 7.5,
+                           b = 7.5,
+                           l = 7.5),
+
       legend.background = element_rect(fill = "transparent",
                                        colour = NA),
+
       legend.key = element_rect(fill = "transparent",
                                 colour = NA),
+
       panel.grid.major = element_line("grey75",
                                       size = rel(0.4)),
+
       panel.grid.minor = element_line("grey75",
                                       size = rel(0.15)),
+
       panel.spacing.x = unit(3, "lines"),
-      strip.text = element_text(size = 10,
-                                face = "bold",
-                                margin = margin(t = 7.5, r = 0, b = 5.5, l = 0)),
-      strip.background = element_rect(fill = "gray95",
-                                      color = "transparent",
-                                      size = rel(0.75)),
+
+      strip.text = element_text(
+        size = 10,
+        face = "bold",
+        margin = margin(
+          t = 7.5,
+          r = 0,
+          b = 5.5,
+          l = 0
+        )
+      ),
+
+      strip.background = element_rect(
+        fill = "gray95",
+        color = "transparent",
+        size = rel(0.75)
+      ),
+
       axis.text = element_text(size = 13),
+
       axis.title = element_text(size = 14),
 
-      complete = TRUE )
+      complete = TRUE
+    )
 
-  grid::grid.rect(0.5, 0.5,
-                width=unit(0.99,"npc"),
-                height=unit(0.99,"npc"),
-          gp = grid::gpar(lwd = outer_border,
-                          fill=NA,
-                          col = outer_border_color))
+  grid::grid.rect(
+    x = 0.5,
+    y = 0.5,
+    width = unit(0.99, "npc"),
+    height = unit(0.99, "npc"),
+    gp = grid::gpar(lwd = outer_border,
+                    fill = NA,
+                    col = outer_border_color)
+  )
 
-  if(inner_border == FALSE) {
+  if (inner_border == FALSE) {
     ret <- ret + theme(panel.border = element_rect(colour = NA),
                        axis.ticks = element_blank())
-    grid::grid.rect(0.5, 0.5,
-                width=unit(0.99,"npc"),
-                height=unit(0.99,"npc"),
-          gp = grid::gpar(lwd = outer_border,
-                          fill=NA,
-                          col = outer_border_color))
+
+    grid::grid.rect(
+      0.5,
+      0.5,
+      width = unit(0.99, "npc"),
+      height = unit(0.99, "npc"),
+      gp = grid::gpar(
+        lwd = outer_border,
+        fill = NA,
+        col = outer_border_color
+      )
+    )
   }
 
-  if(outer_border == FALSE) {
+  if (outer_border == FALSE) {
     ret <- theme_foundationfp() %+replace%
-    theme(
-     panel.border = element_rect(colour = "gray25", size = rel(1.20)),
-      plot.margin = margin(7.5, 7.5, 7.5, 7.5),
-      legend.background = element_rect(fill = "transparent",
-                                       colour = NA),
-      legend.key = element_rect(fill = "transparent",
-                                colour = NA),
-      panel.grid.major = element_line("grey75",
-                                      size = rel(0.4)),
-      panel.grid.minor = element_line("grey75",
-                                      size = rel(0.15)),
-      panel.spacing.x = unit(3, "lines"),
-      strip.text = element_text(size = 10,
-                                face = "bold",
-                                margin = margin(t = 7.5, r = 0, b = 5.5, l = 0)),
-      strip.background = element_rect(fill = "gray95",
-                                      color = "transparent",
-                                      size = rel(0.75)),
-      axis.text = element_text(size = 13),
-      axis.title = element_text(size = 14),
+      theme(
+        panel.border = element_rect(colour = "gray25", size = rel(1.20)),
+        plot.margin = margin(7.5, 7.5, 7.5, 7.5),
+        legend.background = element_rect(fill = "transparent",
+                                         colour = NA),
+        legend.key = element_rect(fill = "transparent",
+                                  colour = NA),
+        panel.grid.major = element_line("grey75",
+                                        size = rel(0.4)),
+        panel.grid.minor = element_line("grey75",
+                                        size = rel(0.15)),
+        panel.spacing.x = unit(3, "lines"),
+        strip.text = element_text(
+          size = 10,
+          face = "bold",
+          margin = margin(
+            t = 7.5,
+            r = 0,
+            b = 5.5,
+            l = 0
+          )
+        ),
+        strip.background = element_rect(
+          fill = "gray95",
+          color = "transparent",
+          size = rel(0.75)
+        ),
+        axis.text = element_text(size = 13),
+        axis.title = element_text(size = 14),
 
-      complete = TRUE )
+        complete = TRUE
+      )
 
   }
   ret
@@ -123,20 +160,73 @@ theme_report <- function(inner_border = TRUE, outer_border = TRUE,
 
 #' @rdname theme_pres
 #' @export
-theme_pres <- function () {
+theme_pres <- function (transparent = TRUE) {
 
-  theme_minimal(base_size=14, base_family="Helvetica") %+replace%
+    if(transparent) {
+
+    theme_minimal(base_size = 14, base_family = "Helvetica") %+replace%
+
     theme(
       panel.background  = element_blank(),
-      legend.background = element_rect(fill="transparent", colour=NA),
-      legend.key = element_rect(fill="transparent", colour=NA),
-      axis.title.y = element_text(vjust = 1.05, angle = 90),
-      axis.title.x = element_text(hjust = 0.975),
+
+      legend.background = element_rect(fill = "transparent",
+                                       colour = NA),
+
+      legend.key = element_rect(fill = "transparent",
+                                colour = NA),
+
+      legend.title = element_text(size = rel(0.8)),
+
+      plot.title = element_text(size = rel(1.4)),
+
+      axis.title.y = element_text(size = rel(1.2),
+                                  vjust = 1.05,
+                                  angle = 90),
+
+      axis.title.x = element_text(size = rel(1.2),
+                                  hjust = 0.975),
+
       plot.margin = unit(c(0.75, 0.75, 0.5, 0.75), "cm"),
-     panel.grid.major = element_line("grey55", size = rel(0.25)),
-      panel.grid.minor = element_line("grey60",size = rel(0.05)),
+
+      panel.grid.major = element_line("grey55",
+                                      size = rel(0.25)),
+
+      panel.grid.minor = element_line("grey60",
+                                      size = rel(0.05)),
       complete = TRUE
     )
+    } else {
+
+    theme_minimal(base_size = 14, base_family = "Helvetica") %+replace%
+
+    theme(
+
+      legend.key = element_rect(fill = "transparent",
+                                colour = NA),
+
+      legend.title = element_text(size = rel(0.8)),
+
+      plot.title = element_text(size = rel(1.4)),
+
+      axis.title.y = element_text(size = rel(1.2),
+                                  vjust = 1.05,
+                                  angle = 90),
+
+      axis.title.x = element_text(size = rel(1.2),
+                                  hjust = 0.975),
+
+      plot.margin = unit(c(0.75, 0.75, 0.5, 0.75), "cm"),
+
+      panel.grid.major = element_line("grey55",
+                                      size = rel(0.25)),
+
+      panel.grid.minor = element_line("grey60",
+                                      size = rel(0.05)),
+      complete = TRUE
+    )
+
+
+    }
 }
 
 #' @rdname theme_pub
